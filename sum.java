771a3505e0eda3e65243fae2dfefc1d6fa705baa
ruchi <ruchi@briskminds.com>
2023-05-11 12:10:14 +0530

    added message in sum.java

--- sum.java
+++ sum.java
@@ -2,6 +2,6 @@ public class Sum{
     public static void main(String [] main){
         int a =  10;
         int b = 20 ;
-        System.out.println('Sum is  :' + sum);
+        System.out.println("Sum is  :"  + sum); 
     }
 }
