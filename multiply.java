a22fd6dd85d2b0edbce9d05543b2c5617c08ed99
ruchi <ruchi@briskminds.com>
2023-05-11 15:14:02 +0530

    modifiy multiply.java

--- multiply.java
+++ multiply.java
@@ -2,6 +2,8 @@ public class multiply {
     public static void main(String [] main){
         int a =  10;
         int b = 20 ;
-        System.out.println("Sum is  :"  +(a*b)); 
+        System.out.println("Sum is  :"  +(a*b));
+        System.out.println("Ruchi Chourasia"); 
+
     }
 }
